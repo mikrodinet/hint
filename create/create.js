89988be0e87a5cb6ed698c78df8704990339eaae
mikrodinet <116235742+mikrodinet@users.noreply.github.com>
2023-05-14 22:21:03 +0700

    Update create.js

--- create/create.js
+++ create/create.js
@@ -121,13 +121,13 @@ async function onEncrypt() {
 
   const encrypted = await generateFragment(url, password, hint, useRandomSalt,
       useRandomIv);
-  const output = `https://wahyu9kdl.github.io/link-lock/#${encrypted}`;
+  const output = `https://mikrodinet.github.io/hint/#${encrypted}`;
 
   document.querySelector("#output").value = output;
   highlight("output");
 
   // Adjust "Hidden Bookmark" link
-  document.querySelector("#bookmark").href = `https://wahyu9kdl.github.io/link-lock/hidden/#${encrypted}`;
+  document.querySelector("#bookmark").href = `https://mikrodinet.github.io/hint/hidden/#${encrypted}`;
 
   // Adjust "Open in New Tab" link
   document.querySelector("#open").href = output;
